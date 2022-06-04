d872960cac108b5310212b06d81f7c43ec9899e5
ismaicetin <47919947+ismaicetin@users.noreply.github.com>
2022-06-04 16:31:28 +0300

    s

--- solar_tracker/solar_tracker.ino
+++ solar_tracker/solar_tracker.ino
@@ -9,16 +9,15 @@
 
 //Set how many steps it takes to make a full revolution
 //Divide the degrees per step by 360 to get the steps
-const int stepsPerRevolution =  2048;
+const int stepsPerRevolution =  48;  //motor 48 adım atınca 1 tur dönuyor demek bunu step motor ozelligi belirliyor
+//360/48=7,5  motor 1 step atınca 7,5 derece donuyor
+
 
 int ldrlt = A0; //LDR top left//top
 int ldrrt = A3; //LDR top rigt// bottom
 int ldrld = A2; //LDR down left //left
 int ldrrd = A1; //ldr down rigt//right
 
-
-
-int hassasiyet = 100;    // select the input pin for the potentiometer
 
 
 
@@ -31,8 +30,8 @@ Stepper  ilerigeriStepper = Stepper(stepsPerRevolution, 4, 6, 5, 7);
 void setup() {
   Serial.begin(9600);
   //Set the RPM of the stepper motor
-  sagsolStepper.setSpeed(5);
-  ilerigeriStepper.setSpeed(5);
+  sagsolStepper.setSpeed(50); //  motorun rpm hızı = 50
+  ilerigeriStepper.setSpeed(50);
 
 }
 
@@ -60,27 +59,27 @@ void loop() {
   // int dtime = analogRead(4)/20; // read potentiometers
   // int tol = analogRead(5)/4;
   int dtime = 10;
-  int tol =50;// 20;
+  int tol = 10; // 20;//hassasiyet
 
-  //  int avt = (lt + rt) / 2; // average value top
-  //  int avd = (ld + rd) / 2; // average value down
-  //  int avl = (lt + ld) / 2; // average value left
-  //  int avr = (rt + rd) / 2; // average value right
-
-
+  //    int avt = (lt + rt) / 2; // average value top
+  //    int avd = (ld + rd) / 2; // average value down
+  //    int avl = (lt + ld) / 2; // average value left
+  //    int avr = (rt + rd) / 2; // average value right
 
 
-    int avt = lt; // average value top
-    int avd =  ld  ; // average value down
-    int avl =  rd  ; // average value left
-    int avr = rt; // average value right
- 
- 
-    int dvert = avt - avd; // check the diffirence of up and down
-    int dhoriz = avl - avr;// check the diffirence og left and rigt
 
 
- 
+  int avt = lt; // average value top
+  int avd =  ld  ; // average value down
+  int avl =  rd  ; // average value left
+  int avr = rt; // average value right
+
+
+  int dvert = avt - avd; // check the diffirence of up and down
+  int dhoriz = avl - avr;// check the diffirence og left and rigt
+
+
+
   Serial.print(avt);
   Serial.print(" ");
   Serial.print(avd);
@@ -96,12 +95,12 @@ void loop() {
   if (-1 * tol > dhoriz || dhoriz > tol) // check if the diffirence is in the tolerance else change horizontal angle
   {
     if (avl > avr)
-    {sagsolStepper.step(-5);
+    { sagsolStepper.step(-1);
     }
     else if (avl < avr)
-    {      sagsolStepper.step(5);
+    { sagsolStepper.step(1);
 
-      
+
     }
 
   }
@@ -109,18 +108,20 @@ void loop() {
   if (-1 * tol > dvert || dvert > tol) // check if the diffirence is in the tolerance else change vertical angle
   {
     if (avt > avd)
-    { ilerigeriStepper.step(5);
-      
+    { ilerigeriStepper.step(1);
+
     }
     else if (avt < avd)
     {
-     ilerigeriStepper.step(-5);
+      ilerigeriStepper.step(-1);
     }
   }
 
 
 
 
+
+  //stepMotor.step(24); // motor 24 step dönecek // 24*7,5=180 derece dönecek
 
   delay(dtime);
 
@@ -129,11 +130,7 @@ void loop() {
 
 
 
+//https://www.robocombo.com/blog/icerik/arduino-ile-step-motor-kontrolu#:~:text=Arduino'daki%205V%20
+//https://elektrikelektronikprojeleri.blogspot.com/2015/04/arduino-step-motor-kontrolu.html
 
-
-
-
-//This should make the
-// stepper do a full 360 degrees
-// sagsolStepper.step(stepsPerRevolution);
-// ilerigeriStepper.step(stepsPerRevolution);
+ 
